b0a3d2b6930165546d1d6d7e57ca048f10a5be07
Nikola <bigmecoprog@gmail.com>
2017-05-22 15:20:16 +0300

    refakstart

---
+++ DnevnikN/src/main/java/controlers/Login.java
@@ -0,0 +1,14 @@
+package controlers;
+
+import javafx.event.ActionEvent;
+
+/**
+ * Created by bigme on 22.05.2017.
+ */
+public class Login {
+    public void exit(ActionEvent actionEvent) {
+    }
+
+    public void Go(ActionEvent actionEvent) {
+    }
+}
