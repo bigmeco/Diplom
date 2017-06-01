d3dc1ed93250fa9ce292b7b9b7ccae47f4818b37
Nikola <bigmecoprog@gmail.com>
2017-06-01 16:43:37 +0300

    test 1/6

---
+++ TestDisain/src/main/java/Main.java
@@ -0,0 +1,27 @@
+/**
+ * Created by bigme on 22.05.2017.
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        AnchorPane root = FXMLLoader.load(getClass().getResource("FXML/loginint.fxml"));
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Diplom");
+        primaryStage.setScene(new Scene(root, 300, 180));
+        primaryStage.show();
+    }
+}
---
+++ TestDisain/src/main/java/controlers/Login.java
@@ -0,0 +1,65 @@
+package controlers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.util.HashMap;
+import java.util.Objects;
+
+/**
+ * Created by bigme on 22.05.2017.
+ */
+public class Login {
+
+    @FXML
+    public Label vizobil1;
+    public TextField predmet;
+    public TextField gryp;
+    public Button cozdat;
+    public TextField im;
+    public TextField fm;
+    public TextField ot;
+    public Label vizobil4;
+    public Label vizobil5;
+    public Label vizobil3;
+    public Label vizobil7;
+    public Label vizobil6;
+    public TextField logr;
+    public TextField parr;
+    @FXML
+    private TextField login;
+    @FXML
+    private TextField pasword;
+    @FXML
+    private Label vizobil;
+    @FXML
+    private Button plus;
+
+
+    public void exit(ActionEvent actionEvent) {
+        System.exit(0);
+    }
+
+    public void Go(ActionEvent actionEvent) {
+
+    }
+
+    public void bec(ActionEvent actionEvent) throws Exception {
+        Node source = (Node) actionEvent.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+        stage.hide();
+    }
+
+    public void Dalie(ActionEvent actionEvent) {
+
+    }
+
+    public void Plus(ActionEvent actionEvent) {
+
+    }
+}
