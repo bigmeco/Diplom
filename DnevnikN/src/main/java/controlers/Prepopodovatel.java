045d5db3c44bfae9656293d1f176eb713011715c
Nikola <bigmecoprog@gmail.com>
2017-06-10 13:25:02 +0300

    prdmet  2.5/10

--- DnevnikN/src/main/java/controlers/Prepopodovatel.java
+++ DnevnikN/src/main/java/controlers/Prepopodovatel.java
@@ -1,6 +1,7 @@
 package controlers;
 
 import POJO.Groups;
+import POJO.Predmeti;
 import POJO.Users;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -13,6 +14,7 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 import link.otvet.OtvJgrups;
+import link.otvet.OtvJpredmet;
 import link.otvet.OtvJuser;
 import link.zapros.Otpravka;
 
@@ -26,28 +28,53 @@ public class Prepopodovatel {
 
     @FXML
     private ListView<String> Gruppi;
+    @FXML
+    private ListView<String> Predmeti;
     private static List<Groups> groups;
+     private static List<Users> users;
+    private static List<POJO.Predmeti> predmetis;
 
     @FXML
     public void initialize() {
         Otpravka otpravka = new Otpravka();
         otpravka.OtpGroups();
+        otpravka.OtpUser();
+        otpravka.OtpPredmet();
         OtvJgrups otvJgrups = new OtvJgrups();
         groups = otvJgrups.getGroups();
-        ObservableList<String> usersData = FXCollections.observableArrayList();
+        OtvJuser otvJuser = new OtvJuser();
+        users = otvJuser.getUsers();
+        OtvJpredmet otvJpredmet = new OtvJpredmet();
+        predmetis = otvJpredmet.getPredmetis();
+        ObservableList<String> gruppData = FXCollections.observableArrayList();
+        ObservableList<String> PredmetiData = FXCollections.observableArrayList();
         for (int x = 0; x < groups.size(); x++) {
-            usersData.add(groups.get(x).getGruppa());
+            gruppData.add(groups.get(x).getGruppa());
         }
 
-        Gruppi.setItems(usersData);
+
+
+        Gruppi.setItems(gruppData);
         Gruppi.getSelectionModel().selectedItemProperty().addListener(
                 (observable, oldValue, newValue) -> {
+                    String[] f = new String[0];
                     for (int x = 0; x < groups.size(); x++) {
                         if (newValue == groups.get(x).getGruppa()) {
-                            System.out.println(groups.get(x).getPredmeti());
+                            f = groups.get(x).getPredmeti().trim().split(",");
                         }
+                        System.out.println(f[x]);
                     }
+
+                    for (int x = 0; x < predmetis.size(); x++) {
+                       //if(){
+                        PredmetiData.add(predmetis.get(x).getPredmet());
+
+                       }
+                   // }
+                    Predmeti.setItems(PredmetiData);
+
                 });
+
     }
 
     public void exit(ActionEvent actionEvent) throws Exception {
